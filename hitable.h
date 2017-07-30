3b862f34051915e1c442eb747e3cfba78f6dfd2b
Yaukey <yaukeywang@Yaukeys-MacBook-Pro.local>
2017-07-30 18:26:46 +0800

    Add Sphere class.

--- hitable.h
+++ hitable.h
@@ -16,7 +16,7 @@ struct hit_record
 class hitable
 {
 public:
-    virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) = 0;
+    virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const = 0;
 };
 
 #endif
---
+++ sphere.h
@@ -0,0 +1,52 @@
+// Add by yaukey at 2017-07-30.
+// Sphere base class.
+
+#ifndef SPHEREH
+#define SPHEREH
+
+#include "hitable.h"
+
+class sphere : public hitable
+{
+public:
+    sphere() {}
+    sphere(const vec3& cen, float r) { center = cen; radius = r; }
+
+    virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const;
+
+    vec3 center;
+    float radius;
+};
+
+bool sphere::hit(const ray& r, float t_min, float t_max, hit_record& rec) const
+{
+    vec3 oc = r.origin() - center;
+    float a = dot(r.direction(), r.direction());
+    float b = dot(oc, r.direction());
+    float c = dot(oc, oc) - radius * radius;
+    float discriminant = b * b - a * c;
+    if (discriminant > 0)
+    {
+        float temp = (-b - sqrt(b * b - a * c)) / a;
+        if (temp > t_min && temp < t_max)
+        {
+            rec.t = temp;
+            rec.p = r.point_at_parameter(rec.t);
+            rec.normal = (rec.p - center) / radius;
+            return true;
+        }
+
+        temp = (-b + sqrt(b * b - a * c)) / a;
+        if (temp > t_min && temp < t_max)
+        {
+            rec.t = temp;
+            rec.p = r.point_at_parameter(rec.t);
+            rec.normal = (rec.p - center) / radius;
+            return true;
+        }
+    }
+
+    return false;
+}
+
+#endif
